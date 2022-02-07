ed24bbf52aead135e8e262b297f9bb5340ac33f2
YevhenVK <onegin797@gmail.com>
2022-02-07 20:33:58 +0200

    Морской бой_рандом

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -33,36 +33,80 @@
 
 // 4 Морской бой
 
-var location1 = 3;  // переменная для хранения позиции
-var location2 = 4;  // переменная для хранения позиции
-var location3 = 5;  // переменная для хранения позиции
+// var location1 = 3;  // переменная для хранения позиции
+// var location2 = 4;  // переменная для хранения позиции
+// var location3 = 5;  // переменная для хранения позиции
 
-var guess;          // переменная для номера текущей попытки
-var hits = 0;       // переменная для количества попаданий
-var guesses = 0;    // переменная для количества попыток
-var isSunk = false; // переменная для хранения информации - потоплен корабль или нет 
+// var guess;          // переменная для номера текущей попытки
+// var hits = 0;       // переменная для количества попаданий
+// var guesses = 0;    // переменная для количества попыток
+// var isSunk = false; // переменная для хранения информации - потоплен корабль или нет 
 
-while (isSunk == false) {
-    guess = prompt("Ready, aim, fire! (enter a number 0-6):");
-    if (guess < 0 || guess > 6) {
-        alert("Please enter a valid cell number")
-    } else {
-        guesses = guesses + 1;
+// while (isSunk == false) {
+//     guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+//     if (guess < 0 || guess > 6) {
+//         alert("Please enter a valid cell number")
+//     } else {
+//         guesses = guesses + 1;
 
-        if (guess == location1 || guess == location2 || guess == location3) {
-            alert("HIT");
-            hits = hits + 1;
+//         if (guess == location1 || guess == location2 || guess == location3) {
+//             alert("HIT");
+//             hits = hits + 1;
             
-            if (hits == 3) {
-                isSunk = true;
-                alert("You sank my battleship!");
-            } 
-        } else {
-            alert("MISS")
-        }
-    }
-}
-var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3 / guesses);
-alert(stats);
+//             if (hits == 3) {
+//                 isSunk = true;
+//                 alert("You sank my battleship!");
+//             } 
+//         } else {
+//             alert("MISS")
+//         }
+//     }
+// }
+// var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3 / guesses);
+// alert(stats);
 
-//
+// 5 Морской бой с случайными цифрами
+
+// var randomLoc = Math.floor(Math.random() * 5);
+
+// var location1 = randomLoc;  // переменная для хранения позиции
+// var location2 = location1 + 1;  // переменная для хранения позиции
+// var location3 = location2 + 1;  // переменная для хранения позиции
+
+// var guess;          // переменная для номера текущей попытки
+// var hits = 0;       // переменная для количества попаданий
+// var guesses = 0;    // переменная для количества попыток
+// var isSunk = false; // переменная для хранения информации - потоплен корабль или нет 
+
+// while (isSunk == false) {
+//     guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+//     if (guess < 0 || guess > 6) {
+//         alert("Please enter a valid cell number")
+//     } else {
+//         guesses = guesses + 1;
+
+//         if (guess == location1 || guess == location2 || guess == location3) {
+//             alert("HIT");
+//             hits = hits + 1;
+            
+//             if (hits == 3) {
+//                 isSunk = true;
+//                 alert("You sank my battleship!");
+//             } 
+//         } else {
+//             alert("MISS")
+//         }
+//     }
+// }
+// var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3 / guesses);
+// alert(stats);
+
+// 6 Функции (глава 3)
+
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says WOOF WOOF");
+    } else {
+        console.log(name + " says woof woof");
+    }  
+}
