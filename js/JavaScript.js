64e6beaa2c15c4b8740cfb6a99f6ea94e19767ab
YevhenVK <onegin797@gmail.com>
2022-02-13 09:12:07 +0200

    Update JavaScript.js

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -103,10 +103,40 @@
 
 // 6 Функции (глава 3)
 
-function bark(name, weight) {
-    if (weight > 20) {
-        console.log(name + " says WOOF WOOF");
-    } else {
-        console.log(name + " says woof woof");
-    }  
+// function bark(name, weight) {
+//     if (weight > 20) {
+//         console.log(name + " says WOOF WOOF");
+//     } else {
+//         console.log(name + " says woof woof");
+//     }  
+// }
+// bark("rover", 23);
+// bark("spot", 13);
+// bark("spike", 53);
+// bark("lady", 17);
+
+// 7 Развлечение с магнитом
+
+// function whatShallIWear(temp) {
+//     if (temp < 60) {
+//         console.log("Wear a jacket");
+//     } else if (temp < 70) {
+//         console.log("Wear a sweater");
+//     } else {
+//         console.log("Wear t-shirt");
+//     }
+// }
+// whatShallIWear(50);
+// whatShallIWear(80);
+// whatShallIWear(60);
+
+// 8  А что будет на выходе?
+
+function doIt(param) {
+    param = 2;
 }
+var test = 1;
+doIt(test);
+console.log(test);
+
+// 9
